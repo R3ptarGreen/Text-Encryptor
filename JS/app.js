8d5e69ad8a01a5fb33354f286fb2d10fee7b5e3e
Edgar <jorge260590@hotmail.com>
2023-07-15 09:28:46 -0500

    mejora en el JS y implementaciín de una pequeña iteración como practica

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,44 @@
 const inputText = document.querySelector("#input-text");
 const outputText = document.querySelector("#output-text");
+const defaultMessage = outputText.innerHTML;
 
+//Función para encriptar
 function encrypt() {
-    let text = inputText.value;
-    let resultEncrypt = text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+  let text = inputText.value.trim();
+  let resultEncrypt = text
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-    document.getElementById("output-text").innerHTML = '<textarea readonly id="input-text" class="textarea-output">' + resultEncrypt + '</textarea>';
+    if (text === ""){
+        outputText.innerHTML = defaultMessage;
+        return;
+    }
+    
+  outputText.innerHTML =
+    '<textarea readonly id="input-text" class="textarea-output">' +
+    resultEncrypt +
+    "</textarea>";
 }
-
+//Función para desencriptar
 function decrypt() {
-    let text = inputText.value;
-    let resultDecrypt = text.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  let text = inputText.value.trim();
+  let resultDecrypt = text
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
-    document.getElementById("output-text").innerHTML = '<textarea readonly id="input-text" class="textarea-output">' + resultDecrypt + '</textarea>';
+    if (text === ""){
+        outputText.innerHTML = defaultMessage;
+        return;
+    }
+
+  outputText.innerHTML =
+    '<textarea readonly id="input-text" class="textarea-output">' +
+    resultDecrypt +
+    "</textarea>";
 }
